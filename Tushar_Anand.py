50a728288bda9cf9b061e37a1d718d5f20216865
tushdemort <40672760+tushdemort@users.noreply.github.com>
2023-08-06 22:05:12 +0530

    Upload

---
+++ Tushar_Anand.py
@@ -0,0 +1,32 @@
+import cv2
+import cv2.aruco as aru 
+from dictionaries import dict
+import numpy
+
+def naming(location_temp,id,img):
+    location=[0,0]
+    location[0]=int(location_temp[0])+ 3
+    location[1]=int(location_temp[1])+3
+    location=tuple(location)
+    id=int(id)
+    text='ID='+str(id)
+    cv2.putText(img,text,location, cv2.FONT_HERSHEY_COMPLEX,0.5,color = (0, 0, 255))
+
+def detect(name):
+    for i in dict:
+        aruco_dict = aru.getPredefinedDictionary(i)
+        aruco_param = aru.DetectorParameters()
+        detector = aru.ArucoDetector(aruco_dict, aruco_param)
+        img = cv2.imread(name)
+        markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(img)
+        if markerIds is None:
+            continue
+        else:
+            #aru.drawDetectedMarkers(img, markerCorners, markerIds )
+            aru.drawDetectedMarkers(img, markerCorners)
+            for i in range(len(markerIds)):
+                naming(markerCorners[i][0][0],markerIds[i][0],img)
+            cv2.imshow('miage',img)
+            cv2.waitKey(0)
+
+detect("aruco.png" )
---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+import cv2.aruco as aru 
+dict=[aru.DICT_6X6_50,
+   aru.DICT_4X4_50,
+   aru.DICT_4X4_100,
+   aru.DICT_4X4_250,
+   aru.DICT_4X4_1000,
+   aru.DICT_5X5_50,
+   aru.DICT_5X5_100,
+   aru.DICT_5X5_250,
+   aru.DICT_5X5_1000,
+   aru.DICT_6X6_100,
+   aru.DICT_6X6_250,
+   aru.DICT_6X6_1000,
+   aru.DICT_7X7_50,
+   aru.DICT_7X7_100,
+   aru.DICT_7X7_250,
+   aru.DICT_7X7_1000,]
+
